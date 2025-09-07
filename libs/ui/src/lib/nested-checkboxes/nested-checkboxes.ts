a07b79359a1168cc7ee79594481773dd0da3f8d3
Oleksii Serhiienko <aleksergie@ukr.net>
2025-09-07 12:07:22 +0300

    created nested-checkboxes component

---
+++ libs/ui/src/lib/nested-checkboxes/nested-checkboxes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-nested-checkboxes',
+  imports: [],
+  templateUrl: './nested-checkboxes.html',
+  styleUrl: './nested-checkboxes.scss',
+})
+export class NestedCheckboxes {}
