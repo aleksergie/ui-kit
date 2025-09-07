c2dc43f0d719df6f60231146ad70102b9295d561
Oleksii Serhiienko <aleksergie@ukr.net>
2025-09-07 13:13:30 +0300

    added route config for the nested checkboxes component

--- apps/ui-kit/src/app/app.routes.ts
+++ apps/ui-kit/src/app/app.routes.ts
@@ -1,4 +1,4 @@
 import { Route } from "@angular/router";
 import { NestedCheckboxes } from "libs/ui/src/lib/nested-checkboxes/nested-checkboxes";
 
-export const appRoutes: Route[] = [{ component: NestedCheckboxes }];
+export const appRoutes: Route[] = [{ path: '**', component: NestedCheckboxes }];
