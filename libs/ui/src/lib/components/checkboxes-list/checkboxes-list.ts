03c18c478e4363b98216c8d779cde6c7f8e0ae4d
Oleksii Serhiienko <aleksergie@ukr.net>
2025-09-14 21:30:35 +0300

    added checkboxes list component

---
+++ libs/ui/src/lib/components/checkboxes-list/checkboxes-list.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-checkboxes-list',
+  imports: [],
+  templateUrl: './checkboxes-list.html',
+  styleUrl: './checkboxes-list.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CheckboxesList {}
